3e918f9635791110e31cd69b4d78ae3e7c2540f0
Chris Baird <cab3953@gmail.com>
2023-02-05 09:52:06 -0800

    Updated routes so they are shorter

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,43 +1,42 @@
-const URL = require('../models/URL')
-const validUrl = require('valid-url')
-const shortId = require('shortid')
+const URL = require("../models/URL")
+const validUrl = require("valid-url")
+const shortId = require("shortid")
 
 async function newUrl(req, res) {
   const url = req.body.url_input
-  console.log(req.body)
   const urlCode = shortId.generate()
 
   // Checking if the url is valid
   if (!validUrl.isWebUri(url)) {
     res.status(401).json({
-      error: 'Invalid URL'
+      error: "Invalid URL",
     })
   } else {
     try {
       // Checking if the url is already in the database
       let nanoUrl = await URL.findOne({
-        original_url: url
+        original_url: url,
       })
       if (nanoUrl) {
         res.json({
           original_url: nanoUrl.original_url,
-          short_url: nanoUrl.short_url
+          short_url: nanoUrl.short_url,
         })
       } else {
         // Creating new one if one does not exist and response with the result
         nanoUrl = new URL({
           original_url: url,
-          short_url: urlCode
+          short_url: urlCode,
         })
         await nanoUrl.save()
         res.json({
           original_url: nanoUrl.original_url,
-          short_url: nanoUrl.short_url
+          short_url: nanoUrl.short_url,
         })
       }
     } catch (err) {
       console.error(err)
-      res.status(500).json('Server erorr...')
+      res.status(500).json("Server erorr...")
     }
   }
 }
@@ -45,16 +44,16 @@ async function newUrl(req, res) {
 async function getUrl(req, res) {
   try {
     const urlParams = await URL.findOne({
-      short_url: req.params.short_url
+      short_url: req.params.short_url,
     })
     if (urlParams) {
       return res.redirect(urlParams.original_url)
     } else {
-      return res.status(404).json('No URL found')
+      return res.status(404).json("No URL found")
     }
   } catch (err) {
     console.log(err)
-    res.status(500).json('Server error')
+    res.status(500).json("Server error")
   }
 }
 
